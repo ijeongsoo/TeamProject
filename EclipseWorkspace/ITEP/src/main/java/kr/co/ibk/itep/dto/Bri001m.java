295ba91da0f6ea131a4090ed7f07bf62f330e477
ijeongsoo <quintessence6083@gmail.com>
2018-10-10 23:28:41 +0900

    Session, File autoUpload

---
+++ EclipseWorkspace/ITEP/src/main/java/kr/co/ibk/itep/dto/Bri001m.java
@@ -0,0 +1,48 @@
+package kr.co.ibk.itep.dto;
+
+public class Bri001m {
+	private String brcd   ;
+	private String krn_brm;  
+	private String reg_id;
+	private String reg_dt ;   
+	private String chg_id;
+	private String chg_dt;
+	public String getBrcd() {
+		return brcd;
+	}
+	public void setBrcd(String brcd) {
+		this.brcd = brcd;
+	}
+	public String getKrn_brm() {
+		return krn_brm;
+	}
+	public void setKrn_brm(String krn_brm) {
+		this.krn_brm = krn_brm;
+	}
+	public String getReg_id() {
+		return reg_id;
+	}
+	public void setReg_id(String reg_id) {
+		this.reg_id = reg_id;
+	}
+	public String getReg_dt() {
+		return reg_dt;
+	}
+	public void setReg_dt(String reg_dt) {
+		this.reg_dt = reg_dt;
+	}
+	public String getChg_id() {
+		return chg_id;
+	}
+	public void setChg_id(String chg_id) {
+		this.chg_id = chg_id;
+	}
+	public String getChg_dt() {
+		return chg_dt;
+	}
+	public void setChg_dt(String chg_dt) {
+		this.chg_dt = chg_dt;
+	}     
+	
+	
+}
---
+++ EclipseWorkspace/ITEP/src/main/java/kr/co/ibk/itep/dto/Emp001m.java
@@ -0,0 +1,64 @@
+package kr.co.ibk.itep.dto;
+
+public class Emp001m {
+	private String emn;
+	private String emm;
+	private String blng_brcd;
+	private String auth_cd;
+	private String reg_id;
+	private String reg_dt;
+	private String chg_id;
+	private String chg_dt;
+	
+	public String getEmn() {
+		return emn;
+	}
+	public void setEmn(String emn) {
+		this.emn = emn;
+	}
+	public String getEmm() {
+		return emm;
+	}
+	public void setEmm(String emm) {
+		this.emm = emm;
+	}
+	public String getBlng_brcd() {
+		return blng_brcd;
+	}
+	public void setBlng_brcd(String blng_brcd) {
+		this.blng_brcd = blng_brcd;
+	}
+	public String getAuth_cd() {
+		return auth_cd;
+	}
+	public void setAuth_cd(String auth_cd) {
+		this.auth_cd = auth_cd;
+	}
+	public String getReg_id() {
+		return reg_id;
+	}
+	public void setReg_id(String reg_id) {
+		this.reg_id = reg_id;
+	}
+	public String getReg_dt() {
+		return reg_dt;
+	}
+	public void setReg_dt(String reg_dt) {
+		this.reg_dt = reg_dt;
+	}
+	public String getChg_id() {
+		return chg_id;
+	}
+	public void setChg_id(String chg_id) {
+		this.chg_id = chg_id;
+	}
+	public String getChg_dt() {
+		return chg_dt;
+	}
+	public void setChg_dt(String chg_dt) {
+		this.chg_dt = chg_dt;
+	}
+
+	
+	
+}
